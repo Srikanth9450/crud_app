343bf5554bcc95a94613a9960d71ab67f5647973
Srikanth9450 <84526844+Srikanth9450@users.noreply.github.com>
2021-06-22 23:36:31 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-const Alien = require("./database/model")
-const connectDB = require('./database/connection');
-connectDB();
+const validator = require("validator")
+console.log(validator.isEmail("srikanth@gmail.com.gmail.com"))
+console.log(validator.contains("sri", "i", { ignoreCase: true }))
 
+/* console.log(merge({ ignoreCase: false }, { ignoreCase: true })) */
+console.log(validator.isAfter("12-07-1999", "12/06/1998"));
 
-Alien.find().then((user) => {
-    for (i of user) {
-        console.log(i.name)
-    }
-})
+console.log(validator.isLowercase("this is lowercase"))
